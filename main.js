583e9fc0e3777cece6b0c4287b78a339262bb48a
Goye <karlos.goye@gmail.com>
2016-01-20 16:24:26 -0500

    search completed

---
+++ main.js
@@ -0,0 +1,47 @@
+"use strict";
+/*TODO 
+Search(val, node)
+Insert(val,node)
+Print(node)
+*/
+
+var Node = class {
+    constructor(val) {
+        this.val = val;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+//Initial Object
+let root = new Node(8);
+root.left = new Node(6);
+root.left.left = new Node(3);
+root.left.right = new Node(7);
+root.right = new Node(12);
+root.right.left = new Node(10);
+root.right.right = new Node(13);
+
+//Search function
+function search(val, node) {
+    if (node == null)
+        return false;
+    else if (val === node.val)   
+        return true;
+    else if (val < node.val)
+        return search(val, node.left);
+    else if (val > node.val)
+        return search(val, node.right); 
+}
+let itExists = search(8, root);
+//console.log(itExists);
+//Insert function
+function insert(val, node) {
+    let itExists = search(val, node);
+    if (itExists)
+        return false;
+    else 
+        //Math logic 
+        ;
+} 
+
